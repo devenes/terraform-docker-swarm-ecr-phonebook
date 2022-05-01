d8cc5509fee66ef90d921d18d30917ccea3386d9
Enes <66560757+devenes@users.noreply.github.com>
2022-05-02 00:01:47 +0300

    Create sec.tf

---
+++ sec.tf
@@ -0,0 +1,38 @@
+resource "aws_security_group" "tf-docker-sec-gr" {
+  name = var.docker_sec_gr_name
+
+  tags = {
+    Name = var.docker_sec_gr_name
+  }
+
+  dynamic "ingress" {
+    for_each = var.sg-ports
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  ingress {
+    from_port   = 7946
+    to_port     = 7946
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 4789
+    to_port     = 4789
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
