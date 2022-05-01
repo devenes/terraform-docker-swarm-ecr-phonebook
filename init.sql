5e65a77ad10aae39023bc36cfa59097f6522bd06
Enes <66560757+devenes@users.noreply.github.com>
2022-05-01 16:06:26 +0300

    Create init.sql

---
+++ init.sql
@@ -0,0 +1,12 @@
+CREATE TABLE phonebook_db.phonebook(
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    number VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+INSERT INTO phonebook_db.phonebook (name, number)
+    VALUES
+        ("Enes Turan", "1234567890"),
+        ("Johnny Depp", "2674854"),
+        ("Bruce Wayne", "076543554");
