1c3d041f2f6a0ec1f7f5aaac13bdb47d28b78603
Enes <66560757+devenes@users.noreply.github.com>
2022-05-02 01:41:00 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -165,8 +165,7 @@ resource "aws_instance" "docker-machine-leader-manager" {
   # security_groups      = ["docker-swarm-sec-gr"]
 
   root_block_device {
-    # volume_size = 16
-    volume_size = 8
+    volume_size = 16
   }
 
   tags = {
