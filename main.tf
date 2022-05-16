f5ef13e21d5771dca28946a80997cc2bad84beb1
Enes <66560757+devenes@users.noreply.github.com>
2022-05-16 15:15:03 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -106,55 +106,6 @@ resource "aws_ecr_repository" "ecr-repo" {
   }
 }
 
-resource "aws_iam_role" "ec2fulltoecr" {
-  name = "ec2roletoecr"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      }
-    ]
-  })
-
-  inline_policy {
-    name = "my_inline_policy"
-
-    policy = jsonencode({
-      Version = "2012-10-17"
-      Statement = [
-        {
-          "Effect" : "Allow",
-          "Action" : "ec2-instance-connect:SendSSHPublicKey",
-          "Resource" : "arn:aws:ec2:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:instance/*",
-          "Condition" : {
-            "StringEquals" : {
-              "ec2:osuser" : "ec2-user"
-            }
-          }
-        },
-        {
-          "Effect" : "Allow",
-          "Action" : "ec2:DescribeInstances",
-          "Resource" : "*"
-        }
-      ]
-    })
-  }
-
-  managed_policy_arns = ["arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess"]
-}
-
-resource "aws_iam_instance_profile" "ec2ecr-profile" {
-  name = var.ec2_ecr_iam_profile_name
-  role = aws_iam_role.ec2fulltoecr.name
-}
-
 resource "aws_instance" "docker-machine-leader-manager" {
   ami                    = var.instance_ami
   instance_type          = var.instance_type
@@ -166,6 +117,7 @@ resource "aws_instance" "docker-machine-leader-manager" {
 
   root_block_device {
     volume_size = 16
+    volume_type = "gp2"
   }
 
   tags = {
