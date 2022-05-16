058eeb4d089829921421e19351de7d732c796ae5
Enes <66560757+devenes@users.noreply.github.com>
2022-05-16 15:15:02 +0300

    Create iam.tf

---
+++ iam.tf
@@ -0,0 +1,48 @@
+resource "aws_iam_role" "ec2fulltoecr" {
+  name = "ec2roletoecr"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  inline_policy {
+    name = "my_inline_policy"
+
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          "Effect" : "Allow",
+          "Action" : "ec2-instance-connect:SendSSHPublicKey",
+          "Resource" : "arn:aws:ec2:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:instance/*",
+          "Condition" : {
+            "StringEquals" : {
+              "ec2:osuser" : "ec2-user"
+            }
+          }
+        },
+        {
+          "Effect" : "Allow",
+          "Action" : "ec2:DescribeInstances",
+          "Resource" : "*"
+        }
+      ]
+    })
+  }
+
+  managed_policy_arns = ["arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess"]
+}
+
+resource "aws_iam_instance_profile" "ec2ecr-profile" {
+  name = var.ec2_ecr_iam_profile_name
+  role = aws_iam_role.ec2fulltoecr.name
+}
